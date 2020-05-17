2b7dee8ea32bbeb3155f2d666fb5cf474b8d6900
Joan <joancolmenero@gmail.com>
2020-05-17 11:35:07 +0200

    Creating activity for the list of repositories by organization and adding a startActivity from Splash to that activity

--- app/src/main/java/com/joancolmenerodev/github_organization_searcher/SplashActivity.kt
+++ app/src/main/java/com/joancolmenerodev/github_organization_searcher/SplashActivity.kt
@@ -1,13 +1,15 @@
 package com.joancolmenerodev.github_organization_searcher
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.joancolmenerodev.organization_searcher.feature.organization_list.presentation.RepositoryByOrganizationSearcherActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+        startActivity(Intent(this, RepositoryByOrganizationSearcherActivity::class.java))
         finish()
     }
 
---
+++ organization_searcher/src/main/java/com/joancolmenerodev/organization_searcher/feature/organization_list/presentation/RepositoryByOrganizationSearcherActivity.kt
@@ -0,0 +1,13 @@
+package com.joancolmenerodev.organization_searcher.feature.organization_list.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.joancolmenerodev.organization_searcher.R
+
+class RepositoryByOrganizationSearcherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repository_by_organization_searcher)
+    }
+}
